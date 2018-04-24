691be7778f05e09168e96f6ad7f10072956923d4
Corneliu Cofaru <cornel@oxoaresearch.com>
2018-04-24 10:49:28 +0200

    Small update to example1 to test indentical node support.

--- examples/example_1.py
+++ examples/example_1.py
@@ -11,25 +11,13 @@ def delayed_graph_ex1():
         return x+1
     
     @dask.delayed
-    def bar(x):
-        return x-1
-
-    @dask.delayed
-    def baz(*args):
+    def bar(*args):
         return sum(args)+1
 
-    @dask.delayed
-    def printme(x):
-        print(x)
-        return 0
-
     v1 = foo(1) # return 2
-    v2 = bar(2) # returns 1
-    p1 = printme(".")
-    v3 = baz(v1, v2, p1) # returns 3
-    v4 = baz(v3, v1,1) # return 5
-    v5 = baz(v1,v2,v3,v4) # returns 11
-    return (v5, 12) # DAG and expected result 
+    v2 = foo(1) # returns 2
+    v3 = bar(v1, v2, 1) # returns 6
+    return (v3, 6) # DAG and expected result
 
 
 def delayed_graph_ex2():
@@ -86,7 +74,8 @@ def compute_with_graphchain(dsk):
 
     with dask.set_options(delayed_optimize = gcoptimize):
         result = dsk.compute(cachedir=cachedir,
-                             compression=True)
+                             compression=True,
+                             logfile="stdout")
     return result
 
 
@@ -99,5 +88,5 @@ def test_ex2():
     assert compute_with_graphchain(dsk) == result
 
 if __name__ == "__main__":
-    # test_ex1()
-    test_ex2()
+    test_ex1()
+    #test_ex2()
