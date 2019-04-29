8a1b484b31ecd219310203e44b8a62c3bbbef204
Laurent Sorber <laurent.sorber@gmail.com>
2019-04-29 17:06:37 +0200

    Bump version number

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'radix.ai'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
